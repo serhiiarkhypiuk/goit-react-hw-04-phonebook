dc08fba655053814ec7c61d26bcdb89a81babe58
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-08-16 10:18:51 +0300

    use of localStorage added

---
+++ src/components/atoms/ContactsList/ContactsList.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  width: 65%;
+  font-size: 18px;
+`;
---
+++ src/components/organisms/Form/Form.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledWrapper = styled.div`
+  width: 25vw;
+  max-width: 25vw;
+  margin: 10vh 25vw;
+  padding: 1rem;
+  background-color: rgba(201, 240, 243, 0.7);
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
